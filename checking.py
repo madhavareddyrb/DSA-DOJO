71cea71115a823369cc6bc56cba42e611f519713
madhavareddyrb <madhavareddyrb@gmail.com>
2026-05-29 10:33:19 +0530

    Sum Array and Frequncy

--- checking.py
+++ checking.py
@@ -1,1 +1,14 @@
-print(1<6)
+def sum_arr(arr):
+    sum = 0
+    for i in range(0,len(arr)):
+        print(arr[i], "i")
+        sum += arr[i]
+        print(sum, "sum")
+
+        arr[i] = sum
+        print(arr, "agdgad")
+    return arr
+
+
+arr = [1, 2, 3, 4, 5]
+print(sum_arr(arr))
